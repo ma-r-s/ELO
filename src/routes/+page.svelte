576dd191070c9a6750ad1e229bbdedf8e726da77
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2023-07-27 20:07:43 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Menu from '~icons/material-symbols/menu-rounded';
+	import Info from './Info.svelte';
+</script>
+
+<div class="drawer lg:drawer-open">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+		<!-- Page content here -->
+		<div class="navbar bg-base-300 lg:hidden">
+			<div class="navbar-start">
+				<label for="my-drawer-2" class="btn btn-square btn-ghost">
+					<Menu class="w-6 h-6" />
+				</label>
+			</div>
+			<p class="navbar-center text-2xl font-serif">ELO Everything</p>
+			<div class="navbar-end">
+				<Info />
+			</div>
+		</div>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer-2" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+			<!-- Sidebar content here -->
+			<p class=" text-2xl font-serif p-4">ELO Everything</p>
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
+	</div>
+</div>
---
+++ src/routes/Info.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Info from '~icons/material-symbols/info-outline-rounded';
+</script>
+
+<button class="btn btn-square btn-ghost" onclick="my_modal_5.showModal()">
+	<Info class="w-6 h-6" />
+</button>
+<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+	<form method="dialog" class="modal-box">
+		<h3 class="font-bold text-lg">Hello!</h3>
+		<p class="py-4">Press ESC key or click the button below to close</p>
+		<div class="modal-action">
+			<!-- if there is a button in form, it will close the modal -->
+			<button class="btn">Close</button>
+		</div>
+	</form>
+</dialog>
